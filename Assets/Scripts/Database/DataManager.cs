1781cce92138d2511319c5bf51fa33c1ddd061fa
datht4889 <hoangdat18112003@gmail.com>
2024-06-08 23:49:52 +0700

    ...

--- Assets/Scripts/Database/DataManager.cs
+++ Assets/Scripts/Database/DataManager.cs
@@ -1,12 +1,59 @@
+using System;
+using System.Collections;
 using Firebase;
 using Firebase.Database;
+using Google.MiniJSON;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class DataManager: MonoBehaviour {
-    
+
+    public InputField username;
+    public InputField gold;
+    private string UserID;
+
+    public Text nameText;
+    public Text goldText;
+
+    DatabaseReference dbReference;
     void Start() {
-        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        UserID = SystemInfo.deviceUniqueIdentifier;
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
+    public void CreateUser(){
+        User newUser = new User(username.text, int.Parse(gold.text));
+        string json = JsonUtility.ToJson(newUser);
+
+        dbReference.Child("users").Child(UserID).SetRawJsonValueAsync(json);
+    }
+
+    public IEnumerator GetName(Action<string> onCallback){
+        var userNameData = dbReference.Child("Users").Child(UserID).Child("name").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userNameData.IsCompleted);
+
+        if (userNameData != null){
+            DataSnapshot snapshot = userNameData.Result;
+
+            onCallback.Invoke(snapshot.Value.ToString());
+        }
+    }
+
+    public IEnumerator GetGold(Action<string> onCallback){
+        var userGoldData = dbReference.Child("Users").Child(UserID).Child("gold").GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => userGoldData.IsCompleted);
+
+        if (userGoldData != null){
+            DataSnapshot snapshot = userGoldData.Result;
+
+            onCallback.Invoke(((int)snapshot.Value).ToString());
+        }
+    }
+
+    public void GetUserInfo(){
+
+    }
 }
--- Assets/Scripts/Database/User.cs
+++ Assets/Scripts/Database/User.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class User: MonoBehaviour {
     public string username;
-    public string email;
+    public int gold;
 
     public User() {
     }
 
-    public User(string username, string email) {
+    public User(string username, int gold) {
         this.username = username;
-        this.email = email;
+        this.gold = gold;
     }
 
 }
