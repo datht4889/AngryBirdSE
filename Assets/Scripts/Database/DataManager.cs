dbb9a0f24d30feb4d1f75c60c688da4230f5a4e0
datht4889 <hoangdat18112003@gmail.com>
2024-06-09 22:58:04 +0700

    ...

--- Assets/Scripts/Database/DataManager.cs
+++ Assets/Scripts/Database/DataManager.cs
@@ -9,22 +9,25 @@ using UnityEngine.UI;
 
 public class DataManager : MonoBehaviour
 {
-    public TMP_InputField username;
-    public TMP_InputField password;
+    public TMP_InputField logInUsername;
+    public TMP_InputField logInPassword;
+    public TMP_InputField signUpUsername;
+    public TMP_InputField signUpPassword;
+    public TMP_InputField signUpConfirmPassword;
     // public InputField gold;
 
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI goldText;
-
-    public Canvas logInPage;
-    public Canvas signUpPage;
-    public Canvas forgortPasswordPage;
+    public GameObject logInPage;
+    public GameObject signUpPage;
+    // public GameObject forgortPasswordPage;
+    public TextMeshProUGUI logInFailed;
+    public TextMeshProUGUI signUpFailed;
 
     private string UserID;
     private DatabaseReference dbReference;
 
     void Start()
     {
+        OpenLogIn();
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
         {
             if (task.Result == DependencyStatus.Available)
@@ -38,62 +41,143 @@ public class DataManager : MonoBehaviour
                 Debug.LogError($"Could not resolve all Firebase dependencies: {task.Result}");
             }
         });
-
-        TextMeshProUGUI usernamePlaceholder = username.placeholder as TextMeshProUGUI;
-        usernamePlaceholder.text = "username";
-        TextMeshProUGUI passwordPlaceholder = password.placeholder as TextMeshProUGUI;
-        passwordPlaceholder.text = "password";
     }
 
+
+    public void OpenLogIn(){
+        logInPage.SetActive(true);
+        signUpPage.SetActive(false);
+        // forgortPasswordPage.SetActive(false);
+        TextMeshProUGUI logInUsernamePlaceholder = logInUsername.placeholder as TextMeshProUGUI;
+        logInUsernamePlaceholder.text = "username";
+        TextMeshProUGUI logInPasswordPlaceholder = logInPassword.placeholder as TextMeshProUGUI;
+        logInPasswordPlaceholder.text = "password";
+    }
     public void OpenSignUp(){
-
+        logInPage.SetActive(false);
+        signUpPage.SetActive(true);
+        // forgortPasswordPage.SetActive(false);
+        TextMeshProUGUI signUpUsernamePlaceholder = signUpUsername.placeholder as TextMeshProUGUI;
+        signUpUsernamePlaceholder.text = "username";
+        TextMeshProUGUI signUpPasswordPlaceholder = signUpPassword.placeholder as TextMeshProUGUI;
+        signUpPasswordPlaceholder.text = "password";
     }
 
-    public void OpenForgortPassword(){
+    // public void OpenForgortPassword(){
+    //     logInPage.SetActive(false);
+    //     signUpPage.SetActive(false);
+    //     forgortPasswordPage.SetActive(true);
+    // }
 
-    }
+    public void LogIn(){
+        string enteredUsername = logInUsername.text;
+        string enteredPassword = logInPassword.text;
 
-    public void CreateUser()
-    {
-        string enteredUsername = username.text;
-
-        // Check if the username already exists
-        dbReference.Child("users").OrderByChild("username").EqualTo(enteredUsername).GetValueAsync().ContinueWithOnMainThread(task =>
-        {
-            if (task.IsCompleted)
+        if(string.IsNullOrEmpty(enteredUsername)||string.IsNullOrEmpty(enteredPassword)){
+            // logInFailed.SetActive(true);
+            logInFailed.text = "Username and Password cannot be empty";
+        }else{
+            dbReference.Child("users").OrderByChild("username").EqualTo(enteredUsername).GetValueAsync().ContinueWithOnMainThread(task =>
             {
-                DataSnapshot snapshot = task.Result;
-
-                if (snapshot.Exists)
+                if (task.IsCompleted)
                 {
-                    Debug.LogError("Username already exists. Please choose another username.");
+                    DataSnapshot snapshot = task.Result;
+
+                    if (snapshot.Exists)
+                    {
+                        foreach (var userSnapshot in snapshot.Children)
+                        {
+                            IDictionary userDict = (IDictionary)userSnapshot.Value;
+                            string storedPassword = userDict["password"].ToString();
+
+                            if (storedPassword == enteredPassword)
+                            {
+                                UserID = userSnapshot.Key;
+                                logInFailed.text = "Login successful!";
+                                // Optionally, load user data or transition to another scene
+                            }
+                            else
+                            {
+                                logInFailed.text = "Incorrect password. Please try again.";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        logInFailed.text = "Username does not exist. Please register.";
+                    }
                 }
                 else
                 {
-                    // Username does not exist, proceed with user creation
-                    UserID = Guid.NewGuid().ToString();
-                    User newUser = new User(enteredUsername, password.text, 0);
-                    string json = JsonUtility.ToJson(newUser);
-
-                    dbReference.Child("users").Child(UserID).SetRawJsonValueAsync(json).ContinueWithOnMainThread(task =>
-                    {
-                        if (task.IsCompleted)
-                        {
-                            Debug.Log("Data written successfully!");
-                        }
-                        else
-                        {
-                            Debug.LogError("Error writing data: " + task.Exception);
-                        }
-                    });
+                    logInFailed.text = "Error checking username: " + task.Exception;
                 }
-            }
-            else
-            {
-                Debug.LogError("Error checking username: " + task.Exception);
-            }
-        });
+            });
+        }
     }
+
+    public void SignUp()
+    {
+        string enteredUsername = signUpUsername.text;
+        string enteredPassword = signUpPassword.text;
+        string confirmedPassword = signUpConfirmPassword.text;
+
+        // Check if the username already exists
+        if(string.IsNullOrEmpty(enteredUsername)||string.IsNullOrEmpty(enteredPassword)||string.IsNullOrEmpty(confirmedPassword)){
+            // logInFailed.SetActive(true);
+            signUpFailed.text = "Username and Password cannot be empty";
+        } else{
+            dbReference.Child("users").OrderByChild("username").EqualTo(enteredUsername).GetValueAsync().ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    Debug.Log("task done");
+
+                    if (snapshot.Exists)
+                    {
+                        // Username already exists, display error message
+                        signUpFailed.text = "Username already exists. Please choose another username.";
+                        Debug.Log("exist");
+                    }
+                    else
+                    {
+                        // Check if the password matches the confirmed password
+                        // if (enteredPassword.Equals(confirmedPassword))
+                        // {
+                        //     // Passwords match, proceed with user creation
+                        Debug.Log("else");
+                            UserID = Guid.NewGuid().ToString();
+                            User newUser = new User(enteredUsername, enteredPassword, 0);
+                            string json = JsonUtility.ToJson(newUser);
+
+                            dbReference.Child("users").Child(UserID).SetRawJsonValueAsync(json).ContinueWithOnMainThread(task =>
+                            {
+                                if (task.IsCompleted)
+                                {
+                                    signUpFailed.text = "Sign up successfully!";
+                                }
+                                else
+                                {
+                                    signUpFailed.text = "Error sign up: " + task.Exception;
+                                }
+                            });
+                        // }
+                        // else
+                        // {
+                        //     // Passwords don't match, display error message
+                        //     signUpFailed.text = "Confirm password doesn't match.";
+                        // }
+                    }
+                }
+                else
+                {
+                    signUpFailed.text = "Error checking username: " + task.Exception;
+                    Debug.Log("lmao");
+                }
+            });
+        }
+    }
+
 
     public IEnumerator GetName(Action<string> onCallback)
     {
@@ -129,54 +213,56 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    public void GetUserInfo()
-    {
-        StartCoroutine(GetName((string name) =>
-        {
-            nameText.text = "Username: " + name;
-        }));
-        StartCoroutine(GetGold((int gold) =>
-        {
-            goldText.text = "Gold: " + gold.ToString();
-        }));
-    }
+    // public void GetUserInfo()
+    // {
+    //     StartCoroutine(GetName((string name) =>
+    //     {
+    //         nameText.text = "Username: " + name;
+    //     }));
+    //     StartCoroutine(GetGold((int gold) =>
+    //     {
+    //         goldText.text = "Gold: " + gold.ToString();
+    //     }));
+    // }
 
-    public void UpdateName()
-    {
-        string newName = nameText.text.Replace("Username: ", "");
-        dbReference.Child("users").Child(UserID).Child("username").SetValueAsync(newName).ContinueWithOnMainThread(task =>
-        {
-            if (task.IsCompleted)
-            {
-                Debug.Log("Username updated successfully!");
-            }
-            else
-            {
-                Debug.LogError("Error updating username: " + task.Exception);
-            }
-        });
-    }
+    // public void UpdateName()
+    // {
+    //     string newName = nameText.text.Replace("Username: ", "");
+    //     dbReference.Child("users").Child(UserID).Child("username").SetValueAsync(newName).ContinueWithOnMainThread(task =>
+    //     {
+    //         if (task.IsCompleted)
+    //         {
+    //             Debug.Log("Username updated successfully!");
+    //         }
+    //         else
+    //         {
+    //             Debug.LogError("Error updating username: " + task.Exception);
+    //         }
+    //     });
+    // }
 
-    public void UpdateGold()
-    {
-        string goldStr = goldText.text.Replace("Gold: ", "");
-        if (int.TryParse(goldStr, out int goldValue))
-        {
-            dbReference.Child("users").Child(UserID).Child("gold").SetValueAsync(goldValue).ContinueWithOnMainThread(task =>
-            {
-                if (task.IsCompleted)
-                {
-                    Debug.Log("Gold updated successfully!");
-                }
-                else
-                {
-                    Debug.LogError("Error updating gold: " + task.Exception);
-                }
-            });
-        }
-        else
-        {
-            Debug.LogError("Invalid gold value");
-        }
-    }
+    // public void UpdateGold()
+    // {
+    //     string goldStr = goldText.text.Replace("Gold: ", "");
+    //     if (int.TryParse(goldStr, out int goldValue))
+    //     {
+    //         dbReference.Child("users").Child(UserID).Child("gold").SetValueAsync(goldValue).ContinueWithOnMainThread(task =>
+    //         {
+    //             if (task.IsCompleted)
+    //             {
+    //                 Debug.Log("Gold updated successfully!");
+    //             }
+    //             else
+    //             {
+    //                 Debug.LogError("Error updating gold: " + task.Exception);
+    //             }
+    //         });
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Invalid gold value");
+    //     }
+    // }
+
+
 }
